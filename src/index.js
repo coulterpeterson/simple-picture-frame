a548d575dc371304703da55d2c2f755fff4d28e9
Coulter Peterson <coulterpeterson@users.noreply.github.com>
2023-09-30 08:51:51 -0400

    Updating to accomodate portrait images, better image scaling, and added a window resize info helper.

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                console.log(folder + val);
-                $(".swiper-wrapper").append( "<div class='swiper-slide'><img src='"+ folder + val +"'></div>" );
+                $(".swiper-wrapper").append( "<div class='swiper-slide' data-bg-url='/"+ folder + val +"'></div>" ); // <img src='"+ folder + val +"'>
               } 
         });
 
@@ -26,6 +25,36 @@ $.ajax({
           autoplay: {
             delay: slideshowDelayInMilliseconds,
           },
+          on: {
+            init: function () {
+              // Swap the data-bg-url as background-image style attributes of each slide
+              $('.swiper-slide').each(function(i, obj) {
+                $(this).css('background-image','url('+ $(this).attr('data-bg-url') +')');
+              });
+            },
+          },
         });
     }
 });
+
+// On window resize, show the resize text and update it
+$( window ).on( "resize", function() {
+  var width = $( window ).width();
+  var height = $( window ).height();
+  var r = gcd (width, height);
+
+  $( '.window-resize-info' )
+    .text(`Width: ${ width } Height: ${ height }, ${ width/r }:${ height/r }`)
+    .addClass('visible');
+
+  setTimeout(hideResizeInfo, 5000);
+} );
+
+// Aspect ratio calc function thanks to https://stackoverflow.com/a/1186465
+function gcd (a, b) {
+  return (b == 0) ? a : gcd (b, a%b);
+}
+
+function hideResizeInfo() {
+  $( '.window-resize-info' ).removeClass('visible');
+}
